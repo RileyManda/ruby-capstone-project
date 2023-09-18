d29271eed3dbd82b0e322ed49aa247ae01b2b094
RileyManda <rileymanda69@gmail.com>
2023-09-18 17:32:03 +0200

    Removed redundant table

--- schema.sql
+++ schema.sql
@@ -1,16 +1,6 @@
 -- create database cataglog
 CREATE DATABASE IF NOT EXISTS things_db;
 
--- create table catalog
-CREATE TABLE IF NOT EXISTS `catalog` (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  description varchar(255) NOT NULL,
-  created_at datetime NOT NULL,
-  updated_at datetime NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
 -- Create the Genre table
 CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
