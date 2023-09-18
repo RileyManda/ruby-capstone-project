a5db80f5faccfe39097cf6bfc9e5746688762397
RileyManda <rileymanda69@gmail.com>
2023-09-18 16:07:30 +0200

    Created music_albums schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,21 @@
 -- create database cataglog
 
 CREATE TABLE IF NOT EXISTS `catalog` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  description varchar(255) NOT NULL,
+  created_at datetime NOT NULL,
+  updated_at datetime NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- create table music_albums
+
+CREATE TABLE music_albums (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    artist VARCHAR(255),
+    release_year INT,
+    on_spotify BOOLEAN,
+    item_id INT REFERENCES items(id)
+);
