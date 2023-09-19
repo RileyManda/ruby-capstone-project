9baef7b8d28e7932f143f949087a45c6027bb2e4
RileyManda <rileymanda69@gmail.com>
2023-09-19 12:37:09 +0200

    Iimported classes

---
+++ main.rb
@@ -0,0 +1,7 @@
+require_relative 'author'
+require_relative 'book'
+require_relative 'game_class'
+require_relative 'genre'
+require_relative 'item'
+require_relative 'label'
+require_relative 'music_album'
