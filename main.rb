2f82942925254564d3fcd4ccc678a6911e759984
RileyManda <rileymanda69@gmail.com>
2023-09-19 12:37:54 +0200

    Implemented options for user selection

--- main.rb
+++ main.rb
@@ -5,3 +5,11 @@ require_relative 'genre'
 require_relative 'item'
 require_relative 'label'
 require_relative 'music_album'
+
+def display_options
+  puts 'Options:'
+  puts '1. Add a new book'
+  puts '2. Add a new game'
+  puts '3. List all items'
+  puts '4. Quit'
+end
