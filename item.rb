fb2115ecf33a31dbb5d0f9cb3ce32498fd92da25
RileyManda <rileymanda69@gmail.com>
2023-09-19 11:01:57 +0200

    Implemented options hash to reduce long lines

--- item.rb
+++ item.rb
@@ -1,18 +1,16 @@
 class Item
   attr_accessor :id, :genre, :author, :source, :label, :public_date, :archived, :can_be_archived, :genre_id
 
-  def initialize(id,
-                 genre, author, source,
-                 label, public_date, archived, can_be_archived, genre_id)
-    @id = id
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
-    @public_date = public_date
-    @archived = archived
-    @can_be_archived = can_be_archived
-    @genre_id = genre_id
+  def initialize(options = {})
+    @id = options[:id]
+    @genre = options[:genre]
+    @author = options[:author]
+    @source = options[:source]
+    @label = options[:label]
+    @public_date = options[:public_date]
+    @archived = options[:archived]
+    @can_be_archived = options[:can_be_archived]
+    @genre_id = options[:genre_id]
   end
 
   def can_be_archived?
