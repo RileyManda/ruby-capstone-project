cb06b72b6a64473b44950e85e683ade0f59fd0a1
RileyManda <rileymanda69@gmail.com>
2023-09-19 13:45:35 +0200

    Updated add game method in main.rb and fixed constructor in game.rb

--- main.rb
+++ main.rb
@@ -20,33 +20,27 @@ end
 items = []
 
 loop do
-  # options
   display_options
-
-  # choose an option
   print 'Select an option: '
   choice = gets.chomp.to_i
 
   case choice
   when 1
-  # Add a new book
-  puts 'Adding a new book...'
-  print "Enter author's name: "
-  author_name = gets.chomp
-  print 'Enter publisher: '
-  publisher = gets.chomp
-  print 'Enter cover state (true or false): '
-  cover_state = gets.chomp.downcase == 'true'
-  print 'Enter publish date (YYYY-MM-DD): '
-  publish_date = Date.parse(gets.chomp)
-  author = Author.new(author_name)
-  book = Book.new(author, publisher, cover_state, publish_date)
-  items << book
+    puts 'Adding a new book...'
+    print "Enter author's name: "
+    author_name = gets.chomp
+    print 'Enter publisher: '
+    publisher = gets.chomp
+    print 'Enter cover state (true or false): '
+    cover_state = gets.chomp.downcase == 'true'
+    print 'Enter publish date (YYYY-MM-DD): '
+    publish_date = Date.parse(gets.chomp)
+    author = Author.new(author_name)
+    book = Book.new(author, publisher, cover_state, publish_date)
+    items << book
 
-  puts 'Book added successfully.'
-
+    puts 'Book added successfully.'
   when 2
-    #  Add a new game
     puts 'Adding a new game...'
     print 'Enter title: '
     title = gets.chomp
@@ -54,7 +48,8 @@ loop do
     description = gets.chomp
     print 'Enter last played date (YYYY-MM-DD): '
     last_played_at = Date.parse(gets.chomp)
-    game = Game.new(title, description, last_played_at)
+    game = Game.new(title: title, description: description, last_played_at: last_played_at)
+
     items << game
     puts 'Game added successfully.'
 
@@ -70,22 +65,22 @@ loop do
     music_album = MusicAlbum.new(album_name, can_be_archived, on_spotify)
     items << music_album
     puts 'Music album added successfully.'
- when 4
-  # List all items
-  puts 'Listing all items:'
-  items.each_with_index do |item, index|
-    case item
-    when MusicAlbum
-      puts "#{index + 1}. Music Album - #{item.album_name || 'No Album Name'}"
-    when Book
-      puts "#{index + 1}. Book - Publisher: #{item.publisher || 'No Publisher'} - Cover State: #{item.cover_state ? 'Good' : 'Bad'}"
-    when Game
-      puts "#{index + 1}. Game - #{item.title || 'No Title'}"
-    else
-      puts "#{index + 1}. #{item.class} - No Title"
-    end
-  end
 
+  when 4
+    # List all items
+    puts 'Listing all items:'
+    items.each_with_index do |item, index|
+      case item
+      when MusicAlbum
+        puts "#{index + 1}. Music Album - #{item.album_name || 'No Album Name'}"
+      when Book
+        puts "#{index + 1}. Book - Publisher: #{item.publisher || 'No Publisher'} - Cover State: #{item.cover_state ? 'Good' : 'Bad'}"
+      when Game
+        puts "#{index + 1}. Game - #{item.title || 'No Title'}"
+      else
+        puts "#{index + 1}. #{item.class} - No Title"
+      end
+    end
 
   when 5
     puts 'Exiting the app. Goodbye!'
