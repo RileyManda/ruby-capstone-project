8654acc69ef2002f0fa13372ab41fbb7f03faeaf
rabayarabu <rabu9149@gmail.com>
2023-09-19 14:15:07 +0600

    Add book and label class

---
+++ book.rb
@@ -0,0 +1,18 @@
+class Book
+  attr_accessor :publisher, :cover_state
+
+  def initialize(author, publisher, cover_state, publish_date)
+    super(publish_date)
+    @author = author
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    if @cover_state || super == 'bad'
+      true
+    else
+      false
+    end
+  end
+end
