8c739c68ff71dd4cfbcd88c3edc2783f94820a86
RileyManda <rileymanda69@gmail.com>
2023-09-21 08:54:20 +0200

    Lin lenght|ignore

--- music_helper.rb
+++ music_helper.rb
@@ -28,21 +28,8 @@ def display_music_album_details(album)
 end
 
 # add music album
-def add_music_album(music_albums, _items, genres)
+def add_music_album(music_albums, items, genres)
   puts 'Adding a new music album...'
-
-  album_name, genre_name, on_spotify, can_be_archived = album_info_from_user
-
-  genre = find_or_create_genre(genres, genre_name)
-
-  new_id = music_albums.size + 1
-  music_album = create_music_album(new_id, album_name, on_spotify, can_be_archived, genre)
-
-  save_music_album(music_albums, music_album)
-  puts 'Music album added successfully.'
-end
-
-def album_info_from_user
   print 'Enter album name: '
   album_name = gets.chomp.to_s
   print 'Enter genre name: '
@@ -52,37 +39,28 @@ def album_info_from_user
   print 'Can the album be archived? (true or false): '
   can_be_archived = gets.chomp.downcase == 'true'
 
-  [album_name, genre_name, on_spotify, can_be_archived]
-end
+  genres = load_genres
 
-def find_or_create_genre(genres, genre_name)
   genre = genres.find { |g| g.name == genre_name }
   unless genre
     genre = Genre.new(genres.size + 1, genre_name)
     genres << genre
     save_genres(genres)
   end
-  genre
-end
 
-def create_music_album(id, album_name, on_spotify, can_be_archived, genre)
-  music_album = MusicAlbum.new(id, album_name, on_spotify, can_be_archived, genre)
+  new_id = music_albums.size + 1
+  music_album = MusicAlbum.new(new_id, album_name, on_spotify, can_be_archived, genre)
+
   music_album.can_be_archived = can_be_archived
-  music_album
+  items << music_album
+  music_albums << { 'id' => new_id,
+                    'album' => { 'can_be_archived' => can_be_archived, 'on_spotify' => on_spotify, 'album_name' => album_name,
+                                 'genre_name' => genre_name } }
+
+  save_music_albums(music_albums)
+  puts 'Music album added successfully.'
 end
 
-def save_music_album(music_albums, music_album)
-  music_albums << {
-    'id' => music_album.id,
-    'album' => {
-      'can_be_archived' => music_album.can_be_archived,
-      'on_spotify' => music_album.on_spotify,
-      'album_name' => music_album.album_name,
-      'genre_name' => music_album.genre.name
-    }
-  }
-  save_music_albums(music_albums)
-end
 
 # save music
 # Save music albums to the JSON file
