1696aadcfcf933adc147733a8aac79d7bb94a0fa
RileyManda <rileymanda69@gmail.com>
2023-09-21 13:42:43 +0200

    Fixd issue with incrementing music albums data to items.json

--- write_music.rb
+++ write_music.rb
@@ -91,22 +91,28 @@ end
 
 # save music album to items collection
 def self.save_items(items)
-  items_data = items.map do |item|
-    {
-      'id' => item.id,
-      'album_name' => item.album_name,
-      'genre' => item.genre.name
-    }
+    json_file_path = 'items.json'
+    items_data = []
+
+    if File.exist?(json_file_path)
+      existing_items_data = JSON.parse(File.read(json_file_path))
+      items_data.concat(existing_items_data)
+    end
+
+    items_data += items.map do |item|
+      {
+        'id' => item.id,
+        'album_name' => item.album_name,
+        'genre' => item.genre.name
+      }
+    end
+
+    File.write(json_file_path, JSON.pretty_generate(items_data))
+
+    puts 'Items saved successfully.'
+  rescue JSON::GeneratorError => e
+    puts "Error generating JSON data for 'items.json': #{e.message}"
+  rescue StandardError => e
+    puts "Error saving items: #{e.message}"
   end
-
-  json_file_path = 'items.json'
-
-  File.write(json_file_path, JSON.pretty_generate(items_data))
-
-  puts 'Items saved successfully.'
-rescue JSON::GeneratorError => e
-  puts "Error generating JSON data for 'items.json': #{e.message}"
-rescue StandardError => e
-  puts "Error saving items: #{e.message}"
-end
 
