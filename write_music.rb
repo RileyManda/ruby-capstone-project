63f6845e661d7f350a91fd472196bb56c454f940
RileyManda <rileymanda69@gmail.com>
2023-09-21 14:03:15 +0200

    Genre arguments number fix

--- write_music.rb
+++ write_music.rb
@@ -44,36 +44,70 @@ class WriteMusic
     puts "Error saving music albums: #{e.message}"
   end
 end
-
+# add music album create music album
 def add_music_album(music_albums, items, genres)
   puts 'Adding a new music album...'
-  print 'Enter album name: '
-  album_name = gets.chomp.to_s
-  print 'Enter genre name: '
-  genre_name = gets.chomp.to_s
-  print 'Is the album on Spotify? (true or false): '
-  on_spotify = gets.chomp.downcase == 'true'
-  print 'Can the album be archived? (true or false): '
-  can_be_archived = gets.chomp.downcase == 'true'
+
+  album_name = prompt_for_album_name
+  genre_name = prompt_for_genre_name
+  on_spotify = prompt_for_spotify_status
+  can_be_archived = prompt_for_archiving_status
+
   loaded_genres = LoadMusic.load_genres
-  genre = loaded_genres.find { |g| g.name == genre_name }
-  unless genre
-    genre = Genre.new(genres.size + 1, genre_name)
-    loaded_genres << genre
-    save_genres(loaded_genres)
-  end
+  genre = find_or_create_genre(loaded_genres, genre_name)
 
   new_id = music_albums.size + 1
-  music_album = MusicAlbum.new(new_id, album_name, on_spotify, can_be_archived, genre)
+  music_album = create_music_album(new_id, album_name, on_spotify, can_be_archived, genre)
 
+  add_to_items(items, music_album)
+  add_to_music_albums(music_albums, new_id, album_name, genre_name, on_spotify, can_be_archived)
+
+  puts 'Music album added successfully.'
+end
+
+def prompt_for_album_name
+  print 'Enter album name: '
+  gets.chomp.to_s
+end
+
+def prompt_for_genre_name
+  print 'Enter genre name: '
+  gets.chomp.to_s
+end
+
+def prompt_for_spotify_status
+  print 'Is the album on Spotify? (true or false): '
+  gets.chomp.downcase == 'true'
+end
+
+def prompt_for_archiving_status
+  print 'Can the album be archived? (true or false): '
+  gets.chomp.downcase == 'true'
+end
+
+def find_or_create_genre(genres, genre_name)
+  genre = genres.find { |g| g.name == genre_name }
+  unless genre
+    genre = Genre.new(genres.size + 1, genre_name)
+    genres << genre
+    save_genres(genres)
+  end
+  genre
+end
+
+
+def create_music_album(id, album_name, on_spotify, can_be_archived, genre)
+  music_album = MusicAlbum.new(id, album_name, on_spotify, can_be_archived, genre)
   music_album.can_be_archived = can_be_archived
+  music_album
+end
+
+def add_to_items(items, music_album)
   items << music_album
-
-  puts 'Items array content after adding music album:'
-  puts items.inspect
-
   save_items(items)
+end
 
+def add_to_music_albums(music_albums, new_id, album_name, genre_name, on_spotify, can_be_archived)
   music_albums << {
     'id' => new_id,
     'album' => {
@@ -83,10 +117,9 @@ def add_music_album(music_albums, items, genres)
       'genre_name' => genre_name
     }
   }
-
   save_music_albums(music_albums)
-  puts 'Music album added successfully.'
 end
+
 
 # save music album to items collection
 def self.save_items(items)
