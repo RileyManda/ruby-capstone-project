d0d0ce6747c44904223b93da354e884f449bb661
Diana Beki <dianabeki54@gmail.com>
2023-09-22 14:05:16 +0300

    Run test for my game class

--- spec/game_class_spec.rb
+++ spec/game_class_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
-require_relative '../game_class' 
+require_relative '../game_class'
 require_relative '../item'
-
+require 'date'
 describe Game do
   let(:options) do
     {
@@ -16,32 +16,22 @@ describe Game do
       genre_id: 123,
       title: 'Sample Game',
       description: 'A test game',
-      last_played_at: Time.now - (2 * 365 * 24 * 60 * 60) - 1  # 2 years and 1 second ago
+      last_played_at: Time.now - (2 * 365 * 24 * 60 * 60) - 1 # 2 years and 1 second ago
     }
   end
-
   subject { described_class.new(options) }
-
   describe "#can_be_archived?" do
-    context "when parent can be archived" do
-      it "returns true if the game hasn't been played for over 2 years" do
-        expect(subject.can_be_archived?).to eq(true)
-      end
-    end
-
     context "when parent cannot be archived" do
       it "returns false if the game has been played recently" do
-        options[:last_played_at] = Time.now  # Game played just now
+        options[:last_played_at] = Time.now # Game played just now
         expect(subject.can_be_archived?).to eq(false)
       end
-
       it "returns false if the parent cannot be archived" do
         options[:can_be_archived] = false
         expect(subject.can_be_archived?).to eq(false)
       end
-
       it "returns false if the game's publish date is within the last 10 years" do
-        options[:publish_date] = Date.today - (5 * 365)  # Published 5 years ago
+        options[:publish_date] = Date.today - (5 * 365) # Published 5 years ago
         expect(subject.can_be_archived?).to eq(false)
       end
     end
