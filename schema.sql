00f7263a1f09b4857627cb8b7b8a284159f52a20
RileyManda <rileymanda69@gmail.com>
2023-09-22 22:21:04 +0200

    Removed item from scheam

--- schema.sql
+++ schema.sql
@@ -7,48 +7,35 @@ CREATE TABLE genre (
     name VARCHAR(255) NOT NULL
 );
 
--- Create the Item table
-CREATE TABLE item (
-    id SERIAL PRIMARY KEY,
-    genre_id INT REFERENCES genre(id),
-    author VARCHAR(255) NOT NULL,
-    source VARCHAR(255) NOT NULL,
-    label VARCHAR(255) NOT NULL,
-    publish_date DATE NOT NULL,
-    archived BOOLEAN NOT NULL
-);
-
--- Create the MusicAlbum table
+-- Create table music_album
 CREATE TABLE music_album (
     id SERIAL PRIMARY KEY,
     publish_date DATE,
     on_spotify BOOLEAN,
-    archived BOOLEAN,
     genre_id INT REFERENCES genre(id),
     author VARCHAR(255) NOT NULL,
     label VARCHAR(255) NOT NULL
 );
 
--- Create the Book table
+-- Create table books
 CREATE TABLE book (
     id SERIAL PRIMARY KEY,
     publisher VARCHAR(100),
     cover_state VARCHAR(100),
     publish_date DATE,
-    archived BOOLEAN,
     genre_id INT REFERENCES genre(id),
     author VARCHAR(255) NOT NULL,
     label VARCHAR(255) NOT NULL
 );
 
--- Create the Label table
+-- Create table labels
 CREATE TABLE label (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     color VARCHAR(255)
 );
 
--- Create the Game table
+-- Create table games
 CREATE TABLE game (
     id SERIAL PRIMARY KEY,
     archived BOOLEAN,
@@ -60,10 +47,9 @@ CREATE TABLE game (
     label VARCHAR(255) NOT NULL
 );
 
--- Create the Author table
+-- Create table authors
 CREATE TABLE author (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    item_id INT
+    last_name VARCHAR(30) NOT NULL
 );
